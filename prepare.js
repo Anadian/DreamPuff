3d607afdcee9f6d73f5e4a23dba169c344824855
Anadian <supercamman20@gmail.com>
2017-08-19 13:24:37 -0400

    Last commit with seperate folders.
    lfs support added.
    
    Fixed lfs?
    
    Fixed lfs?
    
    Anything.
    
    Anything.
    
    Removed resources.
    
    Why is objects so big.

---
+++ prepare.js
@@ -0,0 +1,36 @@
+#!/usr/local/bin/node
+/**
+*	@file prepare.js
+*	@brief A script for automating various task pertaining to preparing a release.
+*	@author Anadian
+*	@copyright MIT License
+*/
+const FileSystem = require('fs');
+const CommandLineArgs = require('command-line-args');
+const OptionDefinitions = [
+	{name: 'version', alias: 'V', type: String},
+	{name: 'help', alias: 'h', type: Boolean},
+	{name: 'verbose', alias: 'v', type: Boolean},
+	{name: 'noop', alias: 'n', type: Boolean},
+	{name: 'installdependencies', alias: 'i', type: Boolean},
+	{name: 'setversion', alias: 's'. type: String}
+];
+const Options = CommandLineArgs(OptionDefinitions);
+if(Options.verbose == true) console.log('Options: ', Options);
+
+function SetVersion(major, minor, patch){
+	var version_string = major + '.' + minor + '.' + patch;
+	var PackageBuffer = FileSystem.readFileSync('./package.json');
+	var PackageText = PackageBuffer.toString('utf8');
+	PackageText.replace(/"version": "[^"]",/, ('\"version\": \"' + version_string + '\",'));
+
+if(Options.setversion != null){
+	var strings = Options.setversion.split(/\./);
+	if(Options.verbose) console.log('strings: ', strings);
+	var version = {
+		major: strings[0],
+		minor: strings[1],
+		patch: strings[2]
+	};
+		UpdateSource(version);
+	UpdateMakefile(version);
